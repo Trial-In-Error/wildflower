0f2bfd1aea10feca4479bb82646eba8feb8801ee
Nathaniel Hutchins <nhutch@amazon.com>
2020-12-28 15:55:18 -0800

    tweak gather.js to allow for filter customization

--- gather.js
+++ gather.js
@@ -4,13 +4,15 @@ const copy = require('recursive-copy')
 const { fixSrcPath, fixDestPath, logNoSuchFile } = require('./common')
 
 const promises = []
-const copyOptions = {
+const defaultCopyOptions = {
   dot: true,
   overwrite: true,
   expand: true,
-  filter: function(e) {
-    return !(e.includes('node_modules'))
-  }
+  filter: [
+    '**/**',
+    '!**/node_modules',
+    '!**/node_modules/**'
+  ]
 }
 
 fse.ensureDirSync('./valley/meadows')
@@ -21,9 +23,25 @@ paths
   .forEach((path) => fse.removeSync(`./valley/meadows/${path}`))
 
 meadows.forEach((meadow) => {
-    promises.push(copy(fixSrcPath(meadow.path), fixDestPath(meadow.path), copyOptions)
-      .then(() => console.log(`Copied ${fixSrcPath(meadow.path)} to ${fixDestPath(meadow.path)}`))
-      .catch(logNoSuchFile))
+  // real dumb way of added some exclusion logic
+  // this ADDS your exclusion logic to the base exclusion logic
+  // currently no way to completely overwrite it
+  let filter = defaultCopyOptions.filter
+
+  if (meadow.filter && Array.isArray(meadow.filter)) {
+    // we're starting with a blank array so we don't overwrite the original filter
+    filter = [].concat(filter, meadow.filter)
+  }
+
+  // we have to be careful we don't overwrite the original copyOptions.filter
+  let copyOptions = {
+    ...defaultCopyOptions,
+    filter
+  }
+
+  promises.push(copy(fixSrcPath(meadow.path), fixDestPath(meadow.path), copyOptions)
+    .then(() => console.log(`Copied ${fixSrcPath(meadow.path)} to ${fixDestPath(meadow.path)}`))
+    .catch(logNoSuchFile))
 })
 
 Promise.all(promises).then(() => console.log('Done gathering.')).catch((err) => console.error('Error while gathering:', err))
